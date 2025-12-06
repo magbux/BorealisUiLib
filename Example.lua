138c5bea256cc42dda667a47c5202bfb1d16573d
Ryusai/Borealis <ddahoodfan@gmail.com>
2025-12-06 13:45:02 +0300

    Update URL for BorealisUiLib in Example.lua

--- Example.lua
+++ Example.lua
@@ -1,4 +1,4 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/magbux/BorealisUiLib/refs/heads/main/BorealisOfficialSH"))()
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/magbux/BorealisUiLib/refs/heads/main/BorealisOfficialUiLib"))()
 
 local Window = Library.new("This is a Window")
 
